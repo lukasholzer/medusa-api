f31eaeb0a7b5935ab33f54213e19427a2858ac4c
Lukas Holzer <office@lukasholzer.com>
2018-01-30 18:10:33 +0100

    add create point

--- api/customers/create.ts
+++ api/customers/create.ts
@@ -1,10 +1,53 @@
 import * as uuid from 'uuid';
 import { DynamoDB } from 'aws-sdk';
 
+import ICustomer from '../interfaces/customer.interface';
+import { Error } from 'aws-sdk/clients/mq';
+import { AWSError } from 'aws-sdk/lib/error';
+import { PutItemOutput } from 'aws-sdk/clients/dynamodb';
+
 
 const dB = new DynamoDB.DocumentClient();
 
+interface Item extends ICustomer {
+  createdAt: number;
+  updatedAt: number;
+}
 
 export function create(event, context, callback) {
 
+    const timestamp = new Date().getTime();
+    const data = JSON.parse(event.body)
+
+    if(typeof data.name !== 'string') {
+      console.error('Validation Failed');
+      callback(new Error('Could\'t parse the data!'));
+      return;
+    }
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        id: uuid.v1() as string,
+        name: data.name,
+        createdAt: timestamp,
+        updatedAt: timestamp
+      } as Item
+    };
+
+    dB.put(params, (error: AWSError, result) => {
+      if(error) {
+        console.error(error);
+        callback(new Error('Could\'t create the new Customer.'));
+        return;
+      }
+
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result)
+      };
+      
+      callback(null, response);
+
+    });
 }
---
+++ api/interfaces/customer.interface.ts
@@ -0,0 +1,32 @@
+export default interface ICustomer {
+  id: string;
+  name: string;
+  company: boolean;
+  web?: string;
+  email?: string;
+  avatar?: string;
+  persons?: ICustomerEmployees[];
+  uid?: string;
+  address?: ICustomerAddress;
+  projects?: ICustomerProjects[];
+  
+}
+
+export interface ICustomerEmployees {
+  id: string
+}
+
+export interface ICustomerAddress {
+  street: string;
+  number: string;
+  town: string;
+  zip: number;
+  country: string;
+  countryShort: string;
+}
+
+export interface ICustomerProjects {
+  id: string;
+  name: string;
+}
+
