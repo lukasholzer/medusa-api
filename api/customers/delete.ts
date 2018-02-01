57f128bbe933ea97ada36bee3de4bc755c1c4792
Lukas Holzer <office@lukasholzer.com>
2018-02-02 00:39:25 +0100

    add delete function to api

---
+++ api/customers/delete.ts
@@ -0,0 +1,24 @@
+import { Context, Callback } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+
+import * as dynamoDbLib from '../libs/dynamodb.lib';
+import { success, failure } from "../libs/response.lib";
+
+
+export async function main(event: any, context: Context, callback: Callback) {
+
+  const params: DynamoDB.DocumentClient.DeleteItemInput = {
+    TableName: `${process.env.DYNAMODB_TABLE}-customers`,
+    Key: {
+      id: event.pathParameters.id
+    }
+  };
+
+  try {
+
+    await dynamoDbLib.call('delete', params);
+    callback(null, success({ status: true }));
+  } catch (e) {
+    callback(null, failure({ status: false, error: 'Customer could not be deleted!', debug: e }));
+  }
+}
